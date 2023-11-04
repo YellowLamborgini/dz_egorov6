811a0666b9328f6c2d6040cf5dfa8d27b162fe3c
YellowLamborgini <denisegorov2011@yandex.ru>
2023-11-04 16:22:33 +0300

    Изменение

--- dz_egorov6/Program.cs
+++ dz_egorov6/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿Console.WriteLine("Введите показания температуры");
+int temperature = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("[1]-Из цельсия в фаренгейты");
+Console.WriteLine("[2]-Из фаренгейта в цельсий");
+int type = Convert.ToInt32(Console.ReadLine());
+double result;
+switch (type)
+{
+      case 1:
+        result = (temperature * 9.0) / 5.0 + 32;
+        break;
+      case 2:
+        result =(temperature-32)*5/9 ;
+        break;
+      default:
+        Console.WriteLine("Неверный символ");
+        result = 0;
+        break;
+}
+Console.WriteLine($"Результат преобразования равен:{result} ");
